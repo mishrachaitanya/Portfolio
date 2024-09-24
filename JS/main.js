b62921cd53118bf985f2696ab05a9aa8c7edcaa5
Chaitanya Mishra <mishrachaitanya.35@gmail.com>
2024-09-24 16:43:45 +0530

    Done!

--- JS/main.js
+++ JS/main.js
@@ -11,7 +11,7 @@ menuIcon.onclick = () =>{
 
 /*  Scrolling section  active  */
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a ');
+let navLinks = document.querySelectorAll('header nav a');
 
 
 window.onscroll = () => {
@@ -22,12 +22,41 @@ window.onscroll = () => {
         let id  = sec.getAttribute('id')
 
         if (top >= offset && top < offset + height){
-            navLinks.forEach.apply(links => {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id +']').classList.add('active'); // 52.20Hrs 
-                })
-            }
-        })
+                });
+            };
+        });
 
+/* Sticky Navbar  */
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-    }
+/*  Remove toggle icon and navbar  */
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+    };
+
+/*  Scroll Reveal  */
+ScrollReveal({ 
+    distance: '80px',
+    duration: 1980,
+    delay: 200,
+ });
+
+ ScrollReveal().reveal('.home-content, heading', {origin: 'top' });
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'button' });
+ ScrollReveal().reveal('.home-contact h1, .about-img', { origin:'left' });
+ ScrollReveal().reveal('.home-contact p, .about-content', { origin:'right' });
+
+ /*  Typed JS  */
+const typed = new Typed('.multiple-text',{
+    strings:['Data Scientist','Web Developer','Deep Learning Practitioner',],
+    typeSpeed: 70,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+    // backDelay:
+})
+
